734d0503677cf7da803f847897f01ea2e7dff29e
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2022-09-24 13:52:31 +0200

    Responding to SIGTERM

--- server.js
+++ server.js
@@ -2,15 +2,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 // Subscribe to unhandled rejection of any promise
-process.on('unhandledRejection', (err) => {
-  console.log(`${err.name}. ${err.message}`);
-  process.exit(1);
-});
-
-process.on('uncaughtException', (err) => {
-  console.log(`${err.name}. ${err.message}`);
-  process.exit(1);
-});
 const app = require('./app');
 
 // Connect to the database
@@ -40,3 +31,18 @@ const port = process.env.PORT || 8000;
 const server = app.listen(port, () =>
   console.log(`App is running on port ${port}...`)
 );
+
+process.on('unhandledRejection', (err) => {
+  console.log(`${err.name}. ${err.message}`);
+  server.close(() => process.exit(1));
+});
+
+process.on('uncaughtException', (err) => {
+  console.log(`${err.name}. ${err.message}`);
+  server.close(() => process.exit(1));
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED, Shutting down...');
+  server.close(() => console.log('Process terminated!'));
+});
